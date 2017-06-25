08285330d9d4013eff00e2087e6693b6e097cfcf
Sean Megan <sean.megan88@gmail.com>
2017-06-24 22:22:13 -0400

    Added dummy database sql dump file

---
+++ db-dumps/db_name.sql
@@ -0,0 +1,6 @@
+/* This db_name.sql file is a placeholder.
+Usage:
+Replace this file with a copy of the SQL database you would like to import.
+*Be sure the SQL file has the same name as the actual database.
+ex.) if database name is 'mycoolsite_db' the SQL file should be 'mycoolsite_db.sql'
+*/
